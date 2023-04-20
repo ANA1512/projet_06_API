5d9e453728585ecf1fdc45d53e01dd5249122a95
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2023-04-20 17:28:18 +0200

    Add files via upload

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,13 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
    try {
+       //get token number only
        const token = req.headers.authorization.split(' ')[1];
+       console.log('token:',token);
+       // decoder token
        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+       console.log('decodedToken:' , decodedToken)
+       //  userId and Token must be the same
        const userId = decodedToken.userId;
        req.auth = {
            userId: userId
