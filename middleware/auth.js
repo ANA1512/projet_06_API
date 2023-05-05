1fe4584acef9e3059189bda0b0f27b90710bc30b
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2023-05-05 18:54:02 +0200

    clean console.log

--- middleware/auth.js
+++ middleware/auth.js
@@ -4,10 +4,10 @@ module.exports = (req, res, next) => {
    try {
        //get token number only
        const token = req.headers.authorization.split(' ')[1];
-       console.log('token:',token);
+
        // decoder token
        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-       console.log('decodedToken:' , decodedToken)
+
        //  userId and Token must be the same
        const userId = decodedToken.userId;
        req.auth = {
