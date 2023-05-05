dc0744b0374c7845a599d89bc8d55fa6a2721d65
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2023-05-05 18:47:49 +0200

    add like route

--- routes/sauce.js
+++ routes/sauce.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 //import de controllers sign up
 const sauceCtrl = require("../controllers/sauce");
 
 
 //post
-router.post('/', auth, sauceCtrl.createSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
 //Put
-router.put('/:id', auth,sauceCtrl.modifySauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 //delete
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+//post like
+router.post('/:id/like', auth, sauceCtrl.likeSauce);
 //get
 router.get('/', auth, sauceCtrl.getAllSauces);
 //get:id
-router.get('/:id',auth, sauceCtrl.getOneSauce);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
 
 module.exports = router;
+
