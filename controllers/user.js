568d0e27e2dc14b95ec20dbc565bfdf5f82d43c0
ANA1512 <65736679+ANA1512@users.noreply.github.com>
2023-05-05 18:55:19 +0200

    maj crypto.js

--- controllers/user.js
+++ controllers/user.js
@@ -1,42 +1,58 @@
 //import du model user
 const User = require('../models/user');
 
-// installer et import jsonwebtoken ( authentification)
+// install and import jsonwebtoken ( authentification)
 const jwt = require('jsonwebtoken');
 
 // crypt password
 const bcrypt = require('bcrypt');
 
+//import crypto.js
+const cryptojs =  require('crypto-js');
+
+// import de dotenv
+const dotenv = require('dotenv');
+const result = dotenv.config();
+
 // user entry in BD
 exports.signup = (req, res, next) => {
+
+//crypter email
+const cryptoMail = cryptojs.SHA256(req.body.email, `${process.env.CRYPTO_KEY_MAIL }`).toString();
 
 //Crypter data user with hash
 bcrypt.hash(req.body.password, 10)
 .then(hash => {
   const user = new User({
-    email: req.body.email,
+    email: cryptoMail,
     password: hash
   });
   user.save()
-    .then(() => res.status(201).json({ message: 'User save !' }))
-    .catch(error => res.status(400).json({ error }));
+    .then(() => res.status(201).json({ message: 'Utilisateur crée dans la base de donnée!' }))
+    .catch(error => res.status(400).json({ error: 'Utilisateur non crée' }));
 })
 .catch(error => res.status(500).json({ error }));
 
 
 };
 
+
+
 // Login user
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
+
+  //crypter email
+const cryptoMail = cryptojs.SHA256(req.body.email, `${process.env.CRYPTO_KEY_MAIL }`).toString();
+
+  User.findOne({ email: cryptoMail })
       .then(user => {
           if (!user) {
-              return res.status(401).json({ error: 'User not found !' });
+              return res.status(401).json({ error: ' Utilisateur/mot de passe incorrecte!' });
           }
           bcrypt.compare(req.body.password, user.password)
               .then(valid => {
                   if (!valid) {
-                      return res.status(401).json({ error: 'password incorrect !' });
+                      return res.status(401).json({ error: 'Utilisateur/mot de passe incorrecte !' });
                   }
                   res.status(200).json({
                       userId: user._id,
@@ -47,9 +63,9 @@ exports.login = (req, res, next) => {
                       )
                   });
               })
-              .catch(error => res.status(500).json({ error }));
+              .catch(error =>{   res.status(500).json({ error })});
       })
-      .catch(error => res.status(500).json({ error }));
+      .catch(error => { res.status(500).json({ error })});
 };
 
 
